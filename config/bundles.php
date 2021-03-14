210f49cc7d49fe4e4b14b7aa2638a0dda23686ec
maksstepak <maks.stepak1@gmail.com>
2021-03-14 20:31:08 +0100

    Add CORS

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
